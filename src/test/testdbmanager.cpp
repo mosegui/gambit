d09beb848cbf74396b865a75403dfe8a3d75a969
mosegui <d.mosegui@gmail.com>
2021-01-14 10:05:29 +0100

    document tests for db manager

--- src/test/testdbmanager.cpp
+++ src/test/testdbmanager.cpp
@@ -1,5 +1,19 @@
 #include "testdbmanager.h"
 
+/*
+ * - Manually create database in preparation function
+ *    - List available Dbs and check it is there
+ *    - CreateIssue in table and manually check it is there
+ *    - Get issue ID and check it is correct
+ *    - Get issue title and check it is correct
+ *    - Get issue description and check it is correct
+ *    - Change issue title and check it changes successfully
+ *    - Change issue description and check it changes successfully
+ *    - remove issue and check it is not there any more
+ * - Manually remove the schema/database
+ */
+
+
 void TestDBManager::testStubPass() {
     QVERIFY(true);
 }
