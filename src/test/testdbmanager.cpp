db928191c42c76fa8b2b3abd24b6ec383e3d2f68
mosegui <d.mosegui@gmail.com>
2021-01-14 12:30:45 +0100

    prepare db testing setup and teardown methods and check test DB is created properly

--- src/test/testdbmanager.cpp
+++ src/test/testdbmanager.cpp
@@ -1,23 +1,37 @@
 #include "testdbmanager.h"
 
-/*
- * - Manually create database in preparation function
- *    - List available Dbs and check it is there
- *    - CreateIssue in table and manually check it is there
+
+/*    - CreateIssue in table and manually check it is there
  *    - Get issue ID and check it is correct
  *    - Get issue title and check it is correct
  *    - Get issue description and check it is correct
  *    - Change issue title and check it changes successfully
  *    - Change issue description and check it changes successfully
  *    - remove issue and check it is not there any more
- * - Manually remove the schema/database
  */
 
 
-void TestDBManager::testStubPass() {
-    QVERIFY(true);
+TestDBManager::~TestDBManager()
+{
+    delete dbManager;
 }
 
-void TestDBManager::testStubFail() {
-    QVERIFY(false);
+void TestDBManager::initTestCase()
+{
+    dbManager = new DBManager("localhost", "admin", 3306);
+    dbManager->createNewDB(testDbNanme);
+}
+
+void TestDBManager::testNewDBwasCreated()
+{
+    // Checks whether the test DB was created successfully
+
+    std::vector<QString> databases = dbManager->getDatabases();
+    QVERIFY(std::find(databases.begin(), databases.end(), testDbNanme) != databases.end());
+}
+
+void TestDBManager::cleanupTestCase()
+{
+    dbManager->deleteDB(testDbNanme);
+    dbManager->closeConnection();
 }
--- src/test/testdbmanager.h
+++ src/test/testdbmanager.h
@@ -1,9 +1,20 @@
 #include <QtTest/QtTest>
 
+#include "dbmanager.h"
+
 class TestDBManager: public QObject
 {
     Q_OBJECT
+
+public:
+    QString testDbNanme = "test_db";
+
+    DBManager* dbManager;
+    ~TestDBManager();
+
 private slots:
-    void testStubPass();
-    void testStubFail();
+    void initTestCase();
+    void cleanupTestCase();
+
+    void testNewDBwasCreated();
 };
