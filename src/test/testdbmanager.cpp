d2fff7ed958e1efaa0ad418d762e810e247650db
mosegui <d.mosegui@gmail.com>
2021-01-15 15:14:49 +0100

    some more DB manager tests

--- src/test/testdbmanager.cpp
+++ src/test/testdbmanager.cpp
@@ -1,11 +1,7 @@
 #include "testdbmanager.h"
+#include <QSqlQuery>
 
-
-/*    - CreateIssue in table and manually check it is there
- *    - Get issue ID and check it is correct
- *    - Get issue title and check it is correct
- *    - Get issue description and check it is correct
- *    - Change issue title and check it changes successfully
+/*    - Change issue title and check it changes successfully
  *    - Change issue description and check it changes successfully
  *    - remove issue and check it is not there any more
  */
@@ -20,6 +16,7 @@ void TestDBManager::initTestCase()
 {
     this->dbManager = new DBManager("localhost", "admin", 3306, "test_connection");
     this->dbManager->createNewDB(this->testDbNanme);
+    this->dbManager->connectToDb(this->testDbNanme);
 }
 
 void TestDBManager::testNewDBwasCreated()
@@ -29,6 +26,39 @@ void TestDBManager::testNewDBwasCreated()
     std::vector<QString> databases = this->dbManager->getDatabases();
     QVERIFY(std::find(databases.begin(), databases.end(), this->testDbNanme) != databases.end());
 }
+
+
+void TestDBManager::testNewIssueCreation()
+{
+    // Checks whether a new issue in the DB can be created sucessfully
+
+    QString expectedIssueID = "DUMMY1";
+    QString expectedIssueTitle = "Dummy Test Issue";
+
+    QString issueID;
+    QString issueTitle;
+
+    this->dbManager->createIssue(expectedIssueID.toStdString(), expectedIssueTitle.toStdString());
+
+    QSqlQuery qry(this->dbManager->mDatabase);
+    QString query = "SELECT * FROM overview";
+    QString result;
+
+    qry.prepare(query);
+
+    if (qry.exec())
+    {
+        while (qry.next())
+        {
+            issueID = qry.value(1).toString();
+            issueTitle = qry.value(2).toString();
+        }
+    }
+
+    QVERIFY(issueID == expectedIssueID);
+    QVERIFY(issueTitle == expectedIssueTitle);
+}
+
 
 void TestDBManager::cleanupTestCase()
 {
--- src/test/testdbmanager.h
+++ src/test/testdbmanager.h
@@ -18,4 +18,5 @@ private slots:
     void cleanupTestCase();
 
     void testNewDBwasCreated();
+    void testNewIssueCreation();
 };
